3ddbab4a62093bdf4bab6d629dc4d57522a12b12
Denys Tolkachov <mrmarkutan@gmail.com>
2024-02-08 12:09:04 +0200

    Add enum for step actions

--- src/main/java/executor/service/steps/ClickCss.java
+++ src/main/java/executor/service/steps/ClickCss.java
@@ -1,13 +1,14 @@
 package executor.service.steps;
 
 import executor.service.model.Step;
+import executor.service.utils.StepAction;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ClickCss implements StepExecution {
     @Override
     public String getStepAction() {
-        return ClickCss.class.getSimpleName();
+        return StepAction.CLICK_CSS.label;
     }
 
     @Override
