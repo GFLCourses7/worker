feb40fb4e43cd512f1ba1ab647211b40bef0b70d
Chernov Danylo <enge51874@gmail.com>
2024-03-04 12:11:55 +0200

    Create ExecutionService

---
+++ src/main/java/executor/service/executor/ExecutionService.java
@@ -0,0 +1,30 @@
+package executor.service.executor;
+
+import executor.service.model.Scenario;
+import executor.service.scenario.ScenarioExecutor;
+import executor.service.scenario.ScenarioSourceListenerImpl;
+import org.openqa.selenium.WebDriver;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExecutionService {
+    private static final Logger logger = Logger.getLogger(ExecutionService.class.getName());
+
+    public void execute(WebDriver driver, ScenarioSourceListenerImpl sourceListener, ScenarioExecutor scenarioExecutor) {
+        Scenario scenario = sourceListener.getScenario();
+        try {
+            logger.info("Starting execution for scenario: " + scenario.getName());
+
+            logger.info("Navigating to site: " + scenario.getSite());
+            driver.get(scenario.getSite());
+
+            logger.info("Executing scenario...");
+            scenarioExecutor.execute(scenario, driver);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Exception occurred during scenario execution: ", e);
+        } finally {
+            logger.info("Quitting WebDriver...");
+            driver.quit();
+        }
+    }
+}
