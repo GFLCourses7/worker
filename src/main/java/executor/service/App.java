904a5abc5db8c24cf02fe579592844e155ac81dc
Denys Kravets <denys.lviv2013@gmail.com>
2024-03-22 21:01:59 +0200

    Added Spring Boot support

--- src/main/java/executor/service/App.java
+++ src/main/java/executor/service/App.java
@@ -3,12 +3,14 @@ package executor.service;
 import executor.service.executor.parallelflowexecution.ParallelFlowExecutorService;
 import executor.service.factory.difactory.AbstractFactory;
 import executor.service.factory.difactory.DIFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class App {
 
     public static void main( String[] args ) {
-        new App().start();
+        SpringApplication.run(App.class, args);
     }
 
     public void start() {
