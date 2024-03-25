fa6288ccb9570a122bbc3606ea709aa7f69bd7ea
Chernov Danylo <enge51874@gmail.com>
2024-03-25 19:33:17 +0200

    44. Migrate factory WebDriverInitializer to the Spring

--- src/main/java/executor/service/config/ChromeProxyConfigurerAddon.java
+++ src/main/java/executor/service/config/ChromeProxyConfigurerAddon.java
@@ -2,13 +2,11 @@ package executor.service.config;
 
 import executor.service.model.ProxyConfigHolder;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.function.Supplier;
 
 public class ChromeProxyConfigurerAddon implements ChromeProxyConfigurer {
-
 
     private final Supplier<ProxyConfigFileInitializer> proxyConfigFileInitializerSupplier;
 
