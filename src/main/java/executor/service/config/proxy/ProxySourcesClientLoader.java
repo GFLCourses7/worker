023221edc9788af0cc2581bac472ac4847841268
Yaroslav Kondrasiuk <yvkondrasiuk@gmail.com>
2024-03-27 23:39:08 +0200

    Migrate to Spring ProxySourcesClient service, optimised imports

--- src/main/java/executor/service/config/proxy/ProxySourcesClientLoader.java
+++ src/main/java/executor/service/config/proxy/ProxySourcesClientLoader.java
@@ -4,14 +4,13 @@ import executor.service.model.ProxyConfigHolder;
 import executor.service.config.JsonConfigReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.LinkedBlockingQueue;
 
+@Service
 public class ProxySourcesClientLoader implements ProxySourcesClient {
 
     private static final Logger LOGGER = LogManager.getLogger(ProxySourcesClientLoader.class);
