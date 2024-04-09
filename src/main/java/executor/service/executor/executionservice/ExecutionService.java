c19577a6a40c2dcf14583489fb756b222e840047
Denys Tolkachov <mrmarkutan@gmail.com>
2024-04-09 14:50:30 +0300

    Add scenarioSourceListener instead of scenario in ExecutionService

--- src/main/java/executor/service/executor/executionservice/ExecutionService.java
+++ src/main/java/executor/service/executor/executionservice/ExecutionService.java
@@ -1,9 +1,9 @@
 package executor.service.executor.executionservice;
 
 import executor.service.executor.scenarioexecutor.ScenarioExecutor;
-import executor.service.model.Scenario;
+import executor.service.listener.ScenarioSourceListener;
 import org.openqa.selenium.WebDriver;
 
 public interface ExecutionService {
-    void execute(WebDriver driver, Scenario scenario, ScenarioExecutor scenarioExecutor);
+    void execute(WebDriver driver, ScenarioSourceListener scenarioSourceListener, ScenarioExecutor scenarioExecutor);
 }
