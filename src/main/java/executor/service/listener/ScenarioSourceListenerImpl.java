d398bc23d1b1ac25586b6ec2551da9d72f20aafc
Chernov Danylo <enge51874@gmail.com>
2024-04-09 21:31:49 +0300

    Change ScenarioSourceListenerImpl, add requests to Client-Service.

--- src/main/java/executor/service/listener/ScenarioSourceListenerImpl.java
+++ src/main/java/executor/service/listener/ScenarioSourceListenerImpl.java
@@ -1,45 +1,30 @@
 package executor.service.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import executor.service.model.Scenario;
-import executor.service.config.JsonConfigReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Objects;
 import java.util.concurrent.LinkedBlockingQueue;
 
 @Service
 public class ScenarioSourceListenerImpl implements ScenarioSourceListener {
 
     private static final Logger logger = LogManager.getLogger(ScenarioSourceListenerImpl.class);
-    private static final String SCENARIOS_JSON = "scenarios.json";
     private LinkedBlockingQueue<Scenario> scenarios = new LinkedBlockingQueue<>();
-
-    public ScenarioSourceListenerImpl() {
-        execute();
-    }
-
-    @Override
-    public void execute() {
-        // Look for scenarios.json inside /resources folder
-        byte[] file = null;
-        try {
-            file = Objects.requireNonNull(
-                    getClass().getClassLoader().getResourceAsStream(SCENARIOS_JSON)
-            ).readAllBytes();
-        } catch (IOException e) {
-            logger.error(e);
-        }
-
-        scenarios.addAll(JsonConfigReader.readFile(file, Scenario.class));
-    }
+    private final OkHttpClient client = new OkHttpClient();
+    // TODO: Rename using Client-Server url, move to props.
+    private final String CLIENT_URL = "http://localhost:8081/api/scenario";
 
     @Override
-    public Scenario getScenario() {
-
+    public synchronized Scenario getScenario() {
         Scenario scenario = null;
+
         while (scenario == null) {
             try {
                 scenario = scenarios.take();
@@ -48,13 +33,42 @@ public class ScenarioSourceListenerImpl implements ScenarioSourceListener {
                 Thread.currentThread().interrupt();
             }
         }
-
         return scenario;
     }
 
     @Override
     public void addScenario(Scenario scenario) {
         scenarios.add(scenario);
+    }
+
+    @Override
+    public void notifyListener() {
+        try {
+            scenarios.add(fetchScenario());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Scenario fetchScenario() throws IOException {
+        // TODO: replace with OkHttp Service
+
+        Request request = new Request.Builder()
+                .url(CLIENT_URL + "/get-scenario")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected response code: " + response);
+            }
+
+            String responseBody = response.body().string();
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(responseBody, Scenario.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public LinkedBlockingQueue<Scenario> getScenarios() {
